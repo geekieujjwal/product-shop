a39221d3c140a73861fc99f047cfd3b148b0d5d8
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2024-01-12 10:33:25 +0530

    added: navigate to home page on adding product

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
   const [name, setName] = useState("");
@@ -6,6 +7,7 @@ const AddProduct = () => {
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     if (!name || !price || !category || !company) {
@@ -22,7 +24,7 @@ const AddProduct = () => {
       },
     });
     const data = await result.json();
-    console.log(data);
+    navigate("/");
   };
 
   return (
